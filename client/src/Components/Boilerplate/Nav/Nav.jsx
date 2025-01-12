a64c4358ebc4db92a91bee2b3a74aa09650da11e
sheikhkaheel <sheikhkaheel@gmail.com>
2025-01-12 14:13:59 +0530

    added commit

--- client/src/Components/Boilerplate/Nav/Nav.jsx
+++ client/src/Components/Boilerplate/Nav/Nav.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import './Nav.css';
 import 'animate.css';
 
+//Nav
+
 export default function Nav() {
     const [openMenu, setOpenMenu] = useState(false);
 
